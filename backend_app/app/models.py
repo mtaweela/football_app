5164fa318eb2709891b395fee2cb87ef0163fb97
mtaweela <mohamed.taweela56@gmail.com>
2020-09-14 02:43:05 +0200

    -- edits to models

--- backend_app/app/models.py
+++ backend_app/app/models.py
@@ -1,9 +1,11 @@
 from peewee import (
     MySQLDatabase,
     Model,
+    PrimaryKeyField,
     CharField,
     TextField,
     IntegerField,
+    FloatField,
     ForeignKeyField,
 )
 from app.settings import DB_SETTINGS
@@ -28,15 +30,17 @@ class Club(BaseModel):
 
 
 class Player(BaseModel):
+    id = PrimaryKeyField()
     name = CharField(null=True)
     age = IntegerField(null=True)
     nationality = ForeignKeyField(
-        column_name="nationality_id", field="id", model=Nationality
+        column_name="nationality_id", field="id", model=Nationality, null=True
     )
-    club = ForeignKeyField(column_name="club_id", field="id", model=Club)
+    club = ForeignKeyField(column_name="club_id", field="id", model=Club, null=True)
     photo = TextField(null=True)
     overall = IntegerField(null=True)
     value = IntegerField(null=True)
+    position = CharField(null=True)
 
 
 def create_schema():
